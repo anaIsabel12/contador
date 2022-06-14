41d6d0539728b9f7193724ce3ede55c3f9378c55
anaIsabel12 <anaigrosales@gmail.com>
2022-06-14 10:12:43 +0100

    Feacture: Subir repositorio

---
+++ practicaTDD/test_contador.py
@@ -0,0 +1,24 @@
+import unittest
+
+import contador
+class Test_contador(unittest.TestCase):
+    #Este es el primer test
+    def test1(self):
+    # Condiciones iniciales#
+        contador1 = contador.Contador(0,2,5)
+    #Verificar#
+        self.assertEqual(contador1.inicial,0)
+        self.assertEqual(contador1.incremento,2)
+        self.assertEqual(contador1.limite,5)
+
+
+    def test2(self):
+        
+        c2=contador.Contador(limite=3)
+
+        self.assertEqual(c2.inicial,0)
+        self.assertEqual(c2.incremento,1)
+        self.assertEqual(c2.limite,3)
+
+if __name__=="__main__":
+    unittest.main()

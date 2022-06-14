1a941a88bfa749a2fdb41bcf1fda958e3b46d5f5
anaIsabel12 <anaigrosales@gmail.com>
2022-06-14 10:26:40 +0100

    Feature: Solucionado el fallo creando la clase contador

--- practicaTDD/contador.py
+++ practicaTDD/contador.py
@@ -1,7 +1,7 @@
 class Contador():
 
-    #inicio = 0
-    #def __init__(self,inicial=0,incremento=1,limite=0):
-       # self.inicial = inicial
-       # self.incremento = incremento
-        #self.limite = limite
+    inicio = 0
+    def __init__(self,inicial=0,incremento=1,limite=0):
+       self.inicial = inicial
+       self.incremento = incremento
+       self.limite = limite
